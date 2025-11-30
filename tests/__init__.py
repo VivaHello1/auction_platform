fb89e5ba6e0c365ea13797ef6a100d7e8e0aef3f
VivaHello1 <viva.hello1@gmail.com>
2025-11-30 20:02:26 +0200

    Some changes

--- tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from core.dependency_injection import create_container
-from tests.test_settings import test_settings
-
-test_container = create_container(test_settings)
--- tests/critical/test_schema.py
+++
@@ -1,6 +0,0 @@
-from tests import test_container
-
-
-def test_schema_is_test():
-    postgres_schema = test_container.config.POSTGRES_SCHEMA()
-    assert postgres_schema == 'api_test'
